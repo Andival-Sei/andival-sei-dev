f4fc92322e7c53a4edce4215d357d97c56f2c357
Kirill Barkov <freedomdragon777@gmail.com>
2025-10-21 17:25:09 +0400

    fix: исключить E2E тесты из Vitest конфигурации

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,6 +14,16 @@ export default defineConfig({
     // Включить глобальные API (describe, it, expect)
     globals: true,
 
+    // Исключаем E2E тесты из Vitest
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+      "**/e2e/**",
+    ],
+
     // Покрытие кода
     coverage: {
       provider: "v8",
